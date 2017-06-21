558aba5a37311a316d876016e9d72df881cfc198
Asir <js.asir@gmail.com>
2017-06-21 00:47:54 -0400

    additional changes

--- src/test/java/stepDefinition/WebPageValidation.java
+++ src/test/java/stepDefinition/WebPageValidation.java
@@ -1,13 +1,20 @@
 package stepDefinition;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class WebPageValidation {
+	
+	WebDriver driver = new FirefoxDriver();
+		
 	@Given("^Web page is displayed with Value (\\d+) to Value (\\d+)$")
 	public void web_page_is_displayed_with_Value_to_Value(int arg1, int arg2) throws Throwable {
-	    
+	driver.get("https://www.exercise1.com/values");
+	
 	}
 
 	@When("^Verify the value of \"([^\"]*)\" is \\$\"([^\"]*)\"$")
